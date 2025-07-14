456885436ec1464da7f33ef64c4ffccd182f0a28
Muthama42 <muthamabrian42@gmail.com>
2025-07-14 16:22:36 +0300

    Fix: Move NLTK data download to app.py for Streamlit Cloud reliability

--- sap_ypp_career_navigator/nlp.py
+++ sap_ypp_career_navigator/nlp.py
@@ -1,9 +1,16 @@
 import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import string
+
+import os
+
+nltk_data_dir = os.path.join(os.getcwd(), "nltk_data")
+os.makedirs(nltk_data_dir, exist_ok=True)
+nltk.data.path.append(nltk_data_dir)
+
+nltk.download('punkt', download_dir=nltk_data_dir)
+nltk.download('stopwords', download_dir=nltk_data_dir)
 
 def extract_keywords(text):
     stop_words = set(stopwords.words('english'))
