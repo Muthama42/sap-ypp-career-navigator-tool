2dd85815704736047b0fdce815e73cb9fa67f5d4
Muthama42 <muthamabrian42@gmail.com>
2025-07-14 17:07:55 +0300

    Local changes to nlp.py before pulling

--- sap_ypp_career_navigator/nlp.py
+++ sap_ypp_career_navigator/nlp.py
@@ -1,19 +1,23 @@
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 import string
 
-import os
-
-nltk_data_dir = os.path.join(os.getcwd(), "nltk_data")
-os.makedirs(nltk_data_dir, exist_ok=True)
-nltk.data.path.append(nltk_data_dir)
-
-nltk.download('punkt', download_dir=nltk_data_dir)
-nltk.download('stopwords', download_dir=nltk_data_dir)
+# A basic English stopword list (expand as needed)
+STOPWORDS = set([
+    "i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves",
+    "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their",
+    "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was",
+    "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and",
+    "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between",
+    "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off",
+    "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any",
+    "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so",
+    "than", "too", "very", "can", "will", "just", "don", "should", "now"
+])
 
 def extract_keywords(text):
-    stop_words = set(stopwords.words('english'))
-    tokens = word_tokenize(text)
-    keywords = [w for w in tokens if w.isalpha() and w.lower() not in stop_words and w.lower() not in string.punctuation]
-    return list(set([w.lower() for w in keywords])) 
+    # Remove punctuation and tokenize
+    translator = str.maketrans(string.punctuation, ' ' * len(string.punctuation))
+    clean_text = text.translate(translator)
+    tokens = clean_text.lower().split()
+    # Remove stopwords and non-alpha tokens
+    keywords = [w for w in tokens if w.isalpha() and w not in STOPWORDS]
+    return list(set(keywords)) 
